2613fa053f7d4ebe3207f2782219c74ef375fc65
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-02-01 11:49:45 -0600

    class examples

--- src/examples/01_module/02_int/vars.cpp
+++ src/examples/01_module/02_int/vars.cpp
@@ -1,6 +1,13 @@
+#include "vars.h"
+
 /*
 Function echo_variable returns the incoming parameter
 
 @param int-integer type
 @return return the int parameter
 */
+
+int echo_variable(int num)
+{
+    return num;
+}
--- src/examples/01_module/03_input/input.h
+++ src/examples/01_module/03_input/input.h
@@ -1,3 +1,5 @@
 //header
 /*write function prototype for get_total with one int parameter (units) and
 a double parameter rate*/
+
+int get_total(int units, double rate);

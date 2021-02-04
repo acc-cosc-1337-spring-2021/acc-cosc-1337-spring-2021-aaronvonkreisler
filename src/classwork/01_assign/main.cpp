20a44ed0fb78d75d8dbd9b4bbde6c1e564be1f9f
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-02-04 16:48:34 -0600

    homework 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -8,6 +8,16 @@ using std::cout;
 
 int main()
 {
+	int num;
+	int num1 = 4;
 
+	cout << "Enter a number to start ... \n";
+	cin >> num;
+	int result = multiply_numbers(num); //Multiply num by 5
+
+	cout << "Result: " << result << "\n";
+
+	result = multiply_numbers(num1);
+	cout << "Result 2: " << result; //Will be 20 every time
 	return 0;
 }
